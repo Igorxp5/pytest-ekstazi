4eb8a391767ba51ec67197e8d12732dba77f6add
Janiel <jns3@cin.ufpe.br>
2022-04-04 23:12:16 -0300

    Test insert product

---
+++ pytest_ekstazi/tests/supermarket/tests/test_code_readers.py
@@ -0,0 +1,16 @@
+from itertools import product
+
+
+def test_insert_product():
+    database = Database()
+    product_id = database.insert_product(name='Product name', price=10.5, amount=10)
+
+    assert product_id, 'No product ID generated'
+
+    products = database.search_product_by_name('Product name')
+    assert products, 'product not found by name'
+
+    products = database.search_product_by_id(product_id)
+    assert products, 'Product not found by ID'
+
+    
